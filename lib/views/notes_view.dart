4527075d2ef9b7f1d8890e66b29cb163bc120448
Ahmed Mahmoud <ahmedmahmouddd57@gmail.com>
2023-10-19 01:39:18 +0300

    add note button sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/notes_view_body.dart';
 
+import '../widgets/add_note_button_sheet.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
@@ -8,8 +10,15 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Color.fromARGB(255, 65, 65, 65).withOpacity(0.7),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return const AddNoteButtonSheet();
+            },
+          );
+        },
+        backgroundColor: const Color.fromARGB(255, 65, 65, 65).withOpacity(0.7),
         child: Icon(
           Icons.add,
           color: Colors.grey[400],
